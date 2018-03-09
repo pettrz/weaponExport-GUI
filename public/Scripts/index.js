cdb11512cd48e2b41a903c871ad46243865b0111
Viola Turesson <violaturesson@gmail.com>
2018-03-09 11:56:28 +0100

    added searchdropdown css

--- public/Scripts/index.js
+++ public/Scripts/index.js
@@ -62,3 +62,22 @@ function CreateMap() {
     });
   });
 }
+
+function searchDropdownFunction() {
+    document.getElementById("searchDropdownContent").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("searchDropdownInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("searchDropdownContent");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
