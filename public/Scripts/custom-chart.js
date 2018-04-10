0576a78375a6588105b214bbcd9fd498aee01cdf
Jenny Miderkvist <jennymider@outlook.com>
2018-04-10 18:00:40 +0200

    shows information from database

--- public/Scripts/custom-chart.js
+++ public/Scripts/custom-chart.js
@@ -1,75 +1,99 @@
 //STATISTICS
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014],
-        datasets: [{
-            label: "Miljarder kr/år ",
-            borderColor: 'rgb(255, 99, 132)',
-            data: [3.1, 3.4, 6.5, 7.3, 8.6, 10.4, 9.6, 12.7, 13.5, 13.7, 13.9, 9.8, 11.9, 8.0],
-            borderWidth: 5,
-            // radius: 10,
-            
-            // backgroundColor: '#505050',
-           
-            //  hoverBorderWidth: 5,
-            
-        }]
-    },
+var xhttpStats = new XMLHttpRequest();
+xhttpStats.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
 
-    // Configuration options go here
-    options: {
-      responsive:true,
-      maintainAspectRatio: false,
-      elements: {
-        point: {
-          radius: 7,
-          hoverRadius: 12,
-          mode: 'index',
-          backgroundColor: 'rgb(255, 99, 132)',
+    var xhttpStatsList = JSON.parse(xhttpStats.response);
+
+    CreateStatistic(xhttpStatsList);
+}}
+xhttpStats.open('GET', 'http://localhost:1137/stats', true);
+xhttpStats.send();
+
+
+function CreateStatistic(request) {
+    console.log(request);
+    var years = [];
+    var weapons = [];
+
+    for (i = 0; i < request.length; i++) {
+        years[i] = request[i].year;
+        weapons[i] = request[i].weapons;
+    }
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: years,
+            datasets: [{
+                label: "Miljarder kr/år ",
+                borderColor: 'rgb(255, 99, 132)',
+                data: weapons,
+                borderWidth: 5,
+                // radius: 10,
+                
+                // backgroundColor: '#505050',
+               
+                //  hoverBorderWidth: 5,
+                
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+          responsive:true,
+          maintainAspectRatio: false,
+          elements: {
+            point: {
+              radius: 7,
+              hoverRadius: 12,
+              mode: 'index',
+              backgroundColor: 'rgb(255, 99, 132)',
+            }
+          },
+          legend: {
+            display: false,
+            // position: 'top',
+            // labels: {
+            //   fontColor: "white",
+            //   fontSize: 18,
+            // }
+          },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fontColor: "white",
+                    fontSize: 18,
+                    stepSize: 2,
+                    beginAtZero: true,
+                },
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Miljarder kr',
+                  fontColor: "white",
+                  fontSize: 20,
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    fontColor: "white",
+                    fontSize: 14,
+                    stepSize: 4,
+                    beginAtZero: true,
+                },
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Årtal',
+                  fontColor: "white",
+                  fontSize: 20,
+                }
+            }]
         }
-      },
-      legend: {
-        display: false,
-        // position: 'top',
-        // labels: {
-        //   fontColor: "white",
-        //   fontSize: 18,
-        // }
-      },
-    scales: {
-        yAxes: [{
-            ticks: {
-                fontColor: "white",
-                fontSize: 18,
-                stepSize: 2,
-                beginAtZero: true,
-            },
-            scaleLabel: {
-              display: true,
-              labelString: 'Miljarder kr',
-              fontColor: "white",
-              fontSize: 20,
-            }
-        }],
-        xAxes: [{
-            ticks: {
-                fontColor: "white",
-                fontSize: 14,
-                stepSize: 4,
-                beginAtZero: true,
-            },
-            scaleLabel: {
-              display: true,
-              labelString: 'Årtal',
-              fontColor: "white",
-              fontSize: 20,
-            }
-        }]
-    }
-    }
-});
+        }
+    });
+}
