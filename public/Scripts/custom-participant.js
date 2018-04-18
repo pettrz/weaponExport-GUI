b4682d2160879273627075bb577a64e31d288a53
Jenny Miderkvist <jennymider@outlook.com>
2018-04-18 14:26:39 +0200

    outprint info

--- public/Scripts/custom-participant.js
+++ public/Scripts/custom-participant.js
@@ -11,7 +11,7 @@ xhttpLogos.onreadystatechange = function() {
         logosList.push(xhttpLogosList[i]);
 
         //Outprints all titles of participants
-         console.log(logosList()[i].participantTitle)
+        //  console.log(logosList()[i].participantTitle)
     }
     
     //Creates function for all images
@@ -32,9 +32,9 @@ function viewModelLogo() {
     self.titleInfo = ko.computed(() =>{
         return self.selectedLogo().participantTitle
     });
-    self.statsInfo = ko.computed(()=>{
-        return self.selectedLogo().info
-    });
+    // self.statsInfo = ko.computed(()=>{
+    //     return self.selectedLogo().info
+    // });
     self.linksLogos = ko.computed(function(){
         return self.selectedLogo().LogoLinks
     });
@@ -45,6 +45,18 @@ function viewModelLogo() {
 
 function CreateLogos(request){
 
+    var participantTitle = [];
+    var img = [];
+
+    for (i = 0; i < request.length; i++) {
+        participantTitle[i] = request[i].participantTitle.toString();
+        img[i] = request[i].img;
+
+        // console.log(participantTitle[i])
+        console.log(img[i])
+    }
+
     //Converts image-links to images
+
 
 }
